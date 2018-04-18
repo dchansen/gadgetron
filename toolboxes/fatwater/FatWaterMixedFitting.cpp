d7330b08458cf7d55b56bb93ffe5de1b8c0f60dd
David Christoffer Hansen <dae.hansen@gmail.com>
2018-04-18 16:21:17 +0200

    Begun adding mixed fitting

---
+++ toolboxes/fatwater/FatWaterMixedFitting.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dch on 18/04/18.
+//
+
+#include "FatWaterMixedFitting.h"
---
+++ toolboxes/fatwater/FatWaterMixedFitting.h
@@ -0,0 +1,16 @@
+//
+// Created by dch on 18/04/18.
+//
+
+#ifndef GADGETRON_FATWATERMIXEDFITTING_H
+#define GADGETRON_FATWATERMIXEDFITTING_H
+
+
+
+class FatWaterMixedFitting {
+
+};
+
+
+
+#endif //GADGETRON_FATWATERMIXEDFITTING_H
